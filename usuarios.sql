e06dc39a5c4968efeb7186061af1f333fca5dad3
walter <walterpaco@gmail.com>
2023-05-21 20:48:33 -0400

    Segunda Actualización Api rest Weps

---
+++ usuarios.sql
@@ -0,0 +1,7 @@
+CREATE TABLE usuario (
+  cedula_identidad VARCHAR(20) PRIMARY KEY,
+  nombre VARCHAR(50) NOT NULL,
+  primer_apellido VARCHAR(50) NOT NULL,
+  segundo_apellido VARCHAR(50),
+  fecha_nacimiento DATE NOT NULL
+);
